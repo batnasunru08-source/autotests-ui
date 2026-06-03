07a4b75af9dd6b20071e87bf754f36ae84cc8d85
Batnasun Ivanov <batnasunru08@gmail.com>
2026-06-03 22:31:48 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,20 @@
+def test_user_login():
+    print("Hello!")
+
+
+class TestUserLogin:
+    def test_1(self):
+        ...
+
+    def test_2(self):
+        ...
+
+
+
+
+def test_assert_positive():
+    assert (2 + 2) == 4
+
+def test_assert_negative():
+    four = (2 + 2)
+    assert four == 5, "(2 + 2) != 5"
