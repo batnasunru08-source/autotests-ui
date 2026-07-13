f9a9caa3a8bc4e587d97726731de30c8cc061c35
Batnasun Ivanov <batnasunru08@gmail.com>
2026-07-13 23:40:02 +0300

    Реализация и использование компонента ImageUploadWidgetComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -23,9 +23,7 @@ def test_create_course(courses_list_page:CoursesListPage, create_course_page: Cr
 
     create_course_page.check_disabled_create_course_button()
 
-    create_course_page.check_visible_image_preview_empty_view()
-
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
 
     create_course_page.check_visible_create_course_form(title="", description="", estimated_time="", max_score="0", min_score="0")
 
@@ -35,9 +33,8 @@ def test_create_course(courses_list_page:CoursesListPage, create_course_page: Cr
 
     create_course_page.check_visible_exercises_empty_view()
 
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
 
     create_course_page.fill_create_course_form(title="Playwright", estimated_time="2 weeks", description="Playwright", max_score="100", min_score="10")
 
