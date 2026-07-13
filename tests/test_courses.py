adfce32cfeb4f3dbdef418924337c34e9902d89e
Batnasun Ivanov <batnasunru08@gmail.com>
2026-07-13 22:40:13 +0300

    Реализация и использование компонента CourseViewComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -48,4 +48,6 @@ def test_create_course(courses_list_page:CoursesListPage, create_course_page: Cr
 
     courses_list_page.check_visible_create_course_button()
 
-    courses_list_page.check_visible_course_card(index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks")
+    courses_list_page.course_view.check_visible(
+        index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
+    )
