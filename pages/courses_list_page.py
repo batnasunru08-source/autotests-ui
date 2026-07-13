64c65035b79a9db85c15f9a038c8b2b8809b8cb0
Batnasun Ivanov <batnasunru08@gmail.com>
2026-07-13 22:44:42 +0300

    Реализация и использование компонента CoursesListToolbarViewComponent

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -4,14 +4,13 @@ from components.navigation.sidebar_component import SidebarComponent
 from components.views.empty_view_component import EmptyViewComponent
 from pages.base_page import BasePage
 from components.courses.course_view_component import CourseViewComponent
-
+from components.courses.courses_list_toolbar_view_component import CoursesListToolbarViewComponent
 
 class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
+        self.toolbar_view = CoursesListToolbarViewComponent(page)
 
         self.course_view = CourseViewComponent(page)
 
@@ -19,19 +18,17 @@ class CoursesListPage(BasePage):
         self.navbar = NavbarComponent(page)
         self.empty_view = EmptyViewComponent(page, 'courses-list')
 
-
-    def check_visible_courses_title(self):
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text('Courses')
-
     def check_visible_empty_view(self):
         self.empty_view.check_visible(
             title='There is no results',
             description='Results from the load test pipeline will be displayed here'
         )
 
-    def check_visible_create_course_button(self):
-        expect(self.create_course_button).to_be_visible()
+    # Методы были удалены, т.к. в автотестах будут использоваться методы компонента
 
-    def click_create_course_button(self):
-        self.create_course_button.click()
+
+    def check_visible_empty_view(self):
+        self.empty_view.check_visible(
+            title='There is no results',
+            description='Results from the load test pipeline will be displayed here'
+        )
