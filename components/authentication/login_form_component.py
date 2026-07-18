2ac1f4db194527517f7ea4237a41819c396668f1
Batnasun Ivanov <batnasunru08@gmail.com>
2026-07-18 20:57:08 +0300

    Знакомство с PageFactory

--- components/authentication/login_form_component.py
+++ components/authentication/login_form_component.py
@@ -2,26 +2,26 @@ from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
 
+
 class LoginFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
         self.email_input = page.get_by_test_id("login-form-email-input").locator("input")
         self.password_input = page.get_by_test_id("login-form-password-input").locator("input")
-        self.login_button = page.get_by_test_id('login-page-login-button')
-        self.registration_link = page.get_by_test_id('login-page-registration-link')
-        self.wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
 
     def fill(self, email, password):
         self.email_input.fill(email)
-        self.password_input.fill(password)
-
-    def check_visible(self, email, password):
         expect(self.email_input).to_have_value(email)
+
+        self.password_input.fill(password)
         expect(self.password_input).to_have_value(password)
 
-        self.login_button.click()
+    def check_visible(self, email, password):
+        expect(self.email_input).to_be_visible()
+        expect(self.email_input).to_have_value(email)
 
-        expect(self.wrong_email_or_password_alert).to_be_visible()
-        expect(self.wrong_email_or_password_alert).to_have_text('Wrong email or password')
+        expect(self.password_input).to_be_visible()
+        expect(self.password_input).to_have_value(password)
+
 
--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -13,3 +13,6 @@ def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str
     login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
     login_page.login_form_component.fill(email=email, password=password)
     login_page.login_form_component.check_visible(email=email, password=password)
+
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
