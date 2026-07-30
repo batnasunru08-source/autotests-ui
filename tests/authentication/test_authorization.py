09b9bcf6de1784e2fa3454960af6850331829e84
Batnasun Ivanov <batnasunru08@gmail.com>
2026-07-30 22:59:52 +0300

    Интеграция Allure в автотесты

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -1,12 +1,15 @@
 import pytest
+import allure
 
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION) # Используем enum
 class TestAuthorization:
     @pytest.mark.parametrize(
         "email, password",
@@ -16,13 +19,18 @@ class TestAuthorization:
             ("  ", "password")
         ]
     )
+    @allure.tag(AllureTag.USER_LOGIN)  # Используем enum
+    @allure.title("User login with wrong email or password")  # Добавляем человекочитаемый заголовок
     def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
+        allure.dynamic.title(f"Attempt to login with email: {email}")  # Заголовок через allure.dynamic.title
         login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
         login_page.login_form_component.fill(email=email, password=password)
         login_page.login_form_component.check_visible(email=email, password=password)
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
 
+    @allure.tag(AllureTag.USER_LOGIN)  # Используем enum
+    @allure.title("User login with correct email and password")
     def test_successful_authorization(
             self,
             login_page: LoginPage,
@@ -46,6 +54,8 @@ class TestAuthorization:
         dashboard_page.navbar.check_visible("username")
         dashboard_page.sidebar.check_visible()
 
+    @allure.tag(AllureTag.NAVIGATION)  # Используем enum
+    @allure.title("Navigation from login page to registration page")
     def test_navigate_from_authorization_to_registration(
             self,
             login_page: LoginPage,
