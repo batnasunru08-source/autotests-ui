8adfaa1bdc2d5a5d15508e42d882a72b0aeef53a
Batnasun Ivanov <batnasunru08@gmail.com>
2026-08-09 11:42:08 +0300

    Настройки автотестов с Pydantic

---
+++ config.py
@@ -0,0 +1,32 @@
+# config.py
+from enum import Enum
+
+from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath, BaseModel
+from pydantic_settings import BaseSettings
+
+
+class Browser(str, Enum):
+    WEBKIT = "webkit"
+    FIREFOX = "firefox"
+    CHROMIUM = "chromium"
+
+
+class TestUser(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+
+
+class TestData(BaseModel):
+    image_png_file: FilePath
+
+
+class Settings(BaseSettings):
+    app_url: HttpUrl
+    headless: bool
+    browsers: list[Browser]
+    test_user: TestUser
+    test_data: TestData
+    videos_dir: DirectoryPath
+    tracing_dir: DirectoryPath
+    browser_state_file: FilePath
