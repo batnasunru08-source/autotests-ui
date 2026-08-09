2be60f8856fdf89097339917a8f6d4b126e97823
Batnasun Ivanov <batnasunru08@gmail.com>
2026-08-09 11:39:29 +0300

    Настройки автотестов с Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -3,7 +3,7 @@ from _pytest.fixtures import SubRequest
 from playwright.sync_api import Playwright, Page
 
 from pages.authentication.registration_page import RegistrationPage
-from tools.playwright.pages import initialize_playwright_page
+from tools.playwright.pages import initialize_playwright_pageпше
 
 
 @pytest.fixture
