e4101d64c995c943e19bc4f147eaebb2dd15cc7e
Batnasun Ivanov <batnasunru08@gmail.com>
2026-08-10 23:15:44 +0300

    Параллельный запуск автотестов

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -23,6 +23,7 @@ from tools.routes import AppRoute
 @allure.suite(AllureFeature.AUTHENTICATION)
 @allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthorization:
+    @pytest.mark.xdist_group(name="authorization-group")
     @pytest.mark.parametrize(
         "email, password",
         [
@@ -43,6 +44,7 @@ class TestAuthorization:
         login_page.check_visible_wrong_email_or_password_alert()
 
     @allure.tag(AllureTag.USER_LOGIN)  # Используем enum
+    @pytest.mark.xdist_group(name="authorization-group")
     @allure.title("User login with correct email and password")
     @allure.severity(Severity.BLOCKER)  # Добавили severity
     def test_successful_authorization(
