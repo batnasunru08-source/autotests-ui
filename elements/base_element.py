24bed6138254274abae7f6c57f15148134613bea
Batnasun Ivanov <batnasunru08@gmail.com>
2026-08-11 18:54:40 +0300

    Логирование взаимодействий со страницей

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,5 +1,9 @@
-import allure  # Имопртируем allure
+import allure
 from playwright.sync_api import Page, Locator, expect
+
+from tools.logger import get_logger  # Импортируем get_logger
+
+logger = get_logger("BASE_ELEMENT")  # Инициализируем logger
 
 
 class BaseElement:
@@ -9,30 +13,37 @@ class BaseElement:
         self.locator = locator
 
     @property
-    def type_of(self) -> str:  # Добавили свойство type_of
+    def type_of(self) -> str:
         return "base element"
 
-    # Метод принимает кейворд аргументы (kwargs)
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
-        # Добавляем аргумент nth со значением по умолчанию 0
         locator = self.locator.format(**kwargs)
-        with allure.step(f'Getting locator with "data-testid={locator}" at index "{nth}"'):  # Добавили шаг
-            return self.page.get_by_test_id(locator).nth(nth)  # Теперь выбираем элемент по индексу
+        step = f'Getting locator with "data-testid={locator}" at index "{nth}"'
+
+        with allure.step(step):
+            logger.info(step)  # Добавили логирование
+            return self.page.get_by_test_id(locator).nth(nth)
 
     def click(self, nth: int = 0, **kwargs):
-        with allure.step(f'Clicking {self.type_of} "{self.name}"'):  # Добавили шаг
-            # Добавили аргумент nth и передаем его в get_locator
+        step = f'Clicking {self.type_of} "{self.name}"'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)  # Добавили логирование
             locator.click()
 
     def check_visible(self, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" is visible'):  # Добавили шаг
-            # Добавили аргумент nth и передаем его в get_locator
+        step = f'Checking that {self.type_of} "{self.name}" is visible'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)  # Добавили логирование
             expect(locator).to_be_visible()
 
     def check_have_text(self, text: str, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" has text "{text}"'):  # Добавили шаг
-            # Добавили аргумент nth и передаем его в get_locator
+        step = f'Checking that {self.type_of} "{self.name}" has text "{text}"'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)  # Добавили логирование
             expect(locator).to_have_text(text)
